6f93ebc634e81b56a8471152783d891902f54ab4
kabina <nheo.an@gmail.com>
2022-06-19 19:32:15 +0900

    추가 MBR Table 적용 / Non-DB test data 적용

---
+++ merge.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+
+dflist = []
+
+for file in os.listdir("po"):
+    if file.startswith("서울특별시.txt_변환완료") :
+        df = pd.read_table(f"po/{file}", sep=",", dtype={"우편번호": str, "위도":str, "경도":str})
+        df['경도'] = df['경도'].replace(r'\\n', '', regex=True)
+        dflist.append(df)
+
+dfs = pd.concat(dflist)
+dfs.to_csv("서울특별시_변환완료.csv", index=False)
